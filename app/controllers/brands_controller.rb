6c44ef6cd10a66f680496b4af77d98c60604dd59
Joseph Lee <jhjlee702@gmail.com>
2024-10-07 17:46:10 -0600

    create new and create methods, as well as a private brand_params method

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,4 +2,25 @@ class BrandsController < ApplicationController
   def index
     @brands = Brand.all
   end
+
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(brand_params)
+
+    if @brand.save
+      redirect_to brands_path, notice: 'Brand successfully added!'
+    else
+      flash.now[:notice] = "Brand was not added successfully - try again."
+      render :new
+    end
+  end
+
+  private
+
+  def brand_params
+    params.require(:brand).permit(:name, :country, :photo)
+  end
 end
