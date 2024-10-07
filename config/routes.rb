569878136a3651b395dc7ac814e28fa2da46d897
Joseph Lee <jhjlee702@gmail.com>
2024-10-07 17:22:33 -0600

    create some new routes for brands and for watches

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  resources :watches
+  get '/', to: 'welcome#index'
+
+  get '/brands', to: 'brands#index'
+  get '/brands/new', to: 'brands#new'
+  post '/brands', to: 'brands#create'
+
+  get '/watches', to: 'watches#index'
+  get '/watches/new', to: 'watches#new', as: 'new_watch'
+  post '/watches', to: 'watches#create'
 end
