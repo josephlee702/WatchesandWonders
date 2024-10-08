92f0a0fa45e60efdef09fb1b218b0f51715ea4bb
Joseph Lee <jhjlee702@gmail.com>
2024-10-08 14:49:03 -0600

    create a basic user registration test

---
+++ spec/features/user_registration_spec.rb
@@ -0,0 +1,15 @@
+# spec/features/user_registration_spec.rb
+require 'rails_helper'
+
+RSpec.feature "UserRegistration", type: :feature do
+  scenario "creates a new user" do
+    visit new_user_registration_path
+    fill_in "Full name", with: "John Doe"
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+  end
+end
