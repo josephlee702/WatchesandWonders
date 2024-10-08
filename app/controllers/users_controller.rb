3ab0eb2c71accbb390308a0405644a2c38f72121
Joseph Lee <jhjlee702@gmail.com>
2024-10-08 15:09:05 -0600

    create new and create methods to aid user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: "Welcome! You have signed up successfully."
+    else
+      flash.now[:notice] = "Account was not created successfully. Try again."
+      render root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:full_name, :email_address, :password_digest)
   end
 end
