11e0dc3ac52a55f619a839346156d79410d0f868
Joseph Lee <jhjlee702@gmail.com>
2024-10-08 15:17:09 -0600

    edit users model fields to match user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :username, uniqueness: true, presence: true
-  validates_presence_of :password, require: true
+  validates :full_name, presence: true
+  validates :email_address, uniqueness: true, presence: true
+  validates_presence_of :password_digest, require: true
 end
