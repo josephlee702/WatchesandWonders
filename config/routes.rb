48d60267b84473559d8b9fab906212b5e9e91a84
Joseph Lee <jhjlee702@gmail.com>
2024-10-08 17:42:37 -0600

    change DELETE to GET....don't know how to fix.

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,5 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy', as: 'logout'
+  get '/logout', to: 'sessions#destroy'
 end
