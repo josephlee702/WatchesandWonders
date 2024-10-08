d15643b065a192db3e46e2786fa7304209fc6cdc
Joseph Lee <jhjlee702@gmail.com>
2024-10-08 15:49:12 -0600

    add test to see if user is logged in after registration

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -11,4 +11,23 @@ RSpec.feature "UserRegistration", type: :feature do
 
     expect(page).to have_content("Welcome, John Doe! You have signed up successfully.")
   end
+
+  it "keeps a user logged in after registering" do
+    visit "/"
+
+    click_on "Register as a User"
+
+    full_name = "John Doe"
+    email_address = "test@test.com"
+    password = "test"
+
+    fill_in "Full Name", with: full_name
+    fill_in "Email Address", with: email_address
+    fill_in "Password", with: password
+    fill_in "Confirm Password", with: password
+
+    click_on "Create Account"
+
+    expect(page).to have_content("Welcome, #{full_name}! You have signed up successfully.")
+  end
 end
