eebdfef624bb9fdf10e86887329c57a7a4bf7eda
Joseph Lee <jhjlee702@gmail.com>
2024-10-08 15:10:11 -0600

    adjust fields to fill in

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.feature "UserRegistration", type: :feature do
   scenario "creates a new user" do
     visit new_user_registration_path
-    fill_in "Full name", with: "John Doe"
+    fill_in "Full Name", with: "John Doe"
     fill_in "Email", with: "john@example.com"
     fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
+    fill_in "Confirm Password", with: "password"
+    click_button "Create Account"
 
     expect(page).to have_content("Welcome! You have signed up successfully.")
   end
