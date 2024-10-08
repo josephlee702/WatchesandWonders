cd9d735feb03581872bf3c2663b0fa6b99797f15
Joseph Lee <jhjlee702@gmail.com>
2024-10-08 15:15:53 -0600

    users table added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_30_232300) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_08_204136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_30_232300) do
   create_table "brands", force: :cascade do |t|
     t.string "name"
     t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email_address"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
