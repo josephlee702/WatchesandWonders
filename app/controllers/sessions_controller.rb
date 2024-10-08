66e4a1465e46970ac43298011dff0c1ad26c3186
Joseph Lee <jhjlee702@gmail.com>
2024-10-08 16:52:16 -0600

    add sad path when account is not found

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,14 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email_address: params[:email_address])
-    session[:user_id] = @user.id
-    flash[:success] = "Welcome, #{@user.full_name}!"
-    redirect_to root_path
+
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.full_name}!"
+      redirect_to root_path
+    else
+      redirect_to login_path
+      flash[:alert] = "User Account was not found."
+    end
   end
 end
