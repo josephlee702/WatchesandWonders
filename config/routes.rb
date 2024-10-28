9462b4a01f88bf6cfb41935c87a8ada20cfe321f
Joseph Lee <jhjlee702@gmail.com>
2024-10-28 15:23:23 -0600

    Create controller, view and spec files for About page

--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+
+  get '/about', to: 'static_pages#about'
 end
