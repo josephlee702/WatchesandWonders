08219e70d1d439b8f5f65029091c5991115129a1
Joseph Lee <jhjlee702@gmail.com>
2024-10-29 17:56:20 -0600

    add a @user.id argument so that route loads correctly, as well as edit destroy method to delete current_user, not @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,18 +9,25 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    # require 'pry'; binding.pry
 
     if @user.save
-      redirect_to root_path, notice: "Welcome, #{@user.full_name}! You have signed up successfully."
+      redirect_to profile_path(@user.id), notice: "Welcome, #{@user.full_name}! You have signed up successfully."
       session[:user_id] = @user.id
     else
       redirect_to new_user_registration_path, notice: "Account was not created successfully. Try again."
     end
   end
 
+  def destroy
+    current_user = User.find(session[:user_id])
+    current_user.delete
+    redirect_to root_path
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:full_name, :email_address, :password_digest)
+    params.require(:user).permit(:full_name, :email_address, :password, :password_confirmation)
   end
 end
