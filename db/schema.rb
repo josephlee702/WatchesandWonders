35dad56c1f5434f9724a0dcf414b92329fc863e2
Joseph Lee <jhjlee702@gmail.com>
2024-11-07 21:17:51 -0700

    user_watches migration added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_08_204136) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_08_034719) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_08_204136) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_watches", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "watch_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_watches_on_user_id"
+    t.index ["watch_id"], name: "index_user_watches_on_watch_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "full_name"
     t.string "email_address"
@@ -74,4 +83,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_08_204136) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "user_watches", "users"
+  add_foreign_key "user_watches", "watches"
 end
