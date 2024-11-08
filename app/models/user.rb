793044011743fe2c80cd3f5e6533228cd8ff26ac
Joseph Lee <jhjlee702@gmail.com>
2024-11-07 20:48:29 -0700

    establish m2m relationship with watches through user_watches

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_watches
+  has_many :watches, through: :user_watches
+
   validates :full_name, presence: true
   validates :email_address, uniqueness: true, presence: true
   validates_presence_of :password_digest, require: true
